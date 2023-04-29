b43a7a52529cdcbb551e07f04df0b0c24bf2b902
Realharambe <dogeduck2.0@gmail.com>
2023-04-29 12:33:32 -0700

    changed a bit of stuff

--- theme.js
+++ theme.js
@@ -16,3 +16,41 @@ function toggleTheme() {
   const themeToggle = document.getElementById("theme-toggle");
   themeToggle.addEventListener("click", toggleTheme);
   
+  // JavaScript code
+  let score = 0;
+  let clickMultiplier = 1;
+  
+  const clickButton = document.getElementById("click-button");
+  const scoreDisplay = document.getElementById("score");
+  const upgradeButtons = document.getElementsByClassName("upgrade");
+  const purchasedUpgrades = document.getElementById("purchased-upgrades");
+  
+  function increaseScore() {
+    score += clickMultiplier;
+    scoreDisplay.textContent = score;
+  }
+  
+  clickButton.addEventListener("click", increaseScore);
+  
+  function purchaseUpgrade(event) {
+    const button = event.target;
+    const upgradeCost = parseInt(button.getAttribute("data-cost"));
+    const upgradeMultiplier = parseInt(button.getAttribute("data-multiplier"));
+  
+    if (score >= upgradeCost) {
+      score -= upgradeCost;
+      clickMultiplier *= upgradeMultiplier;
+      scoreDisplay.textContent = score;
+  
+      const upgradeItem = document.createElement("li");
+      upgradeItem.textContent = `Upgrade ${button.id.substr(-1)} (Multiplier: ${upgradeMultiplier}x)`;
+      purchasedUpgrades.appendChild(upgradeItem);
+  
+      button.disabled = true;
+    }
+  }
+  
+  for (let i = 0; i < upgradeButtons.length; i++) {
+    upgradeButtons[i].addEventListener("click", purchaseUpgrade);
+  }
+  
